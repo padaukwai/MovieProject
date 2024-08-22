3462a493790a6c0f8696fa0708c5c593168fa078
IMU <IMU@DESKTOP-FSRQE0S>
2024-08-22 09:55:52 +0630

    Commit 1

---
+++ src/movie/com/MovieDemo.java
@@ -0,0 +1,10 @@
+package movie.com;
+
+public class MovieDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome from Movie System");
+	}
+
+}
